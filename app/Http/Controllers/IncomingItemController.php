13c52f2d5702233c4cd5bd9d277822432288e2a0
budimesa <budihmesa@gmail.com>
2024-10-03 14:10:50 +0700

    update incoming item store api

--- app/Http/Controllers/IncomingItemController.php
+++ app/Http/Controllers/IncomingItemController.php
@@ -5,7 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\IncomingItem;
-
+use App\Models\Inventory;
+use Illuminate\Support\Facades\DB;
 class IncomingItemController extends Controller
 {
     /**
@@ -24,15 +25,14 @@ class IncomingItemController extends Controller
             return response()->json($lastItem, 200);
         }
 
-        return response()->json(['message' => 'No items found'], 404);
+        return response()->json(['message' => 'No items found'], 205);
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
-    {
-        $validated = $request->validate([
+    public function store(Request $request) {
+        $request->validate([
             'incoming_item_code' => 'required|unique:incoming_items,incoming_item_code',
             'supplier_id' => 'required',
             'warehouse_id' => 'required',
@@ -45,13 +45,61 @@ class IncomingItemController extends Controller
             'total_cost' => 'required',
             'notes' => 'nullable',
             'invoice_files' => 'nullable',
+            'details' => 'required|array',
+            'details.*.item_id' => 'required|string',
+            // 'details.*.batch_id' => 'required|string',
+            'details.*.barcode_number' => 'required|numeric',
+            'details.*.gross_weight' => 'required|numeric',
+            'details.*.net_weight' => 'required|numeric',
+            'details.*.unit_price' => 'required|numeric',
+            'details.*.actual_stock' => 'required|numeric',
+            'details.*.total_price' => 'required|numeric',
+            'details.*.labor_cost' => 'required|numeric',
+            'details.*.notes' => 'nullable|string',
+            'details.*.expiry_date' => 'nullable|date',
+            'details.*.transaction_type' => 'nullable|string',
         ]);
 
-        $validated['created_by'] = Auth::id();
+        // Mulai transaction
+        DB::transaction(function () use ($request) {
+            // Insert ke tabel mst_bom_formula
+            $incomingItem = IncomingItem::create([
+                'incoming_item_code' => $request->incoming_item_code,
+                'supplier_id' => $request->supplier_id,
+                'warehouse_id' => $request->warehouse_id,
+                'shipment_date' => $request->shipment_date,
+                'received_date' => $request->received_date,
+                'total_item_price' => $request->total_item_price,
+                'shipping_cost' => $request->shipping_cost,
+                'labor_cost' => $request->labor_cost,
+                'other_fee' => $request->other_fee,
+                'total_cost' => $request->total_cost,
+                'notes' => $request->notes,
+                'invoice_files' => $request->invoice_files,
+                'created_by' => Auth::id(),
+            ]);
+            
+            foreach ($request->details as $detail) {
+                Inventory::create([
+                    'incoming_item_id' => $incomingItem->id,
+                    'item_id' => $detail['item_id'],
+                    // 'batch_id' => $detail['batch_id'],
+                    'barcode_number' => $detail['barcode_number'],
+                    'gross_weight' => $detail['gross_weight'],
+                    'net_weight' => $detail['net_weight'],
+                    'unit_price' => $detail['unit_price'],
+                    'actual_stock' => $detail['actual_stock'],
+                    'total_price' => $detail['total_price'],
+                    'labor_cost' => $detail['labor_cost'],
+                    'notes' => $detail['notes'],
+                    // 'expiry_date' => $detail['expiry_date'],
+                    'transaction_type' => $detail['transaction_type'],
+                    'created_by' => Auth::id(),
+                ]);
+            }
+        });
 
-        $incomingItem = IncomingItem::create($validated);
-
-        return response()->json($incomingItem, 201);
+        return response()->json(['message' => 'Data inserted successfully'], 201);
     }
 
     /**
